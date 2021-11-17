321a94457d14a14a2733d4a032525d9040c82052
Misil <carlossilvamarques.cs@gmail.com>
2021-11-17 16:21:32 +0100

    Updates

---
+++ precache-manifest.ebc5b0dda321ffd809b32f5bb0098de1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4287b7b245fb6e7ff043",
+    "url": "/react-plotly/static/css/main.4379803a.chunk.css"
+  },
+  {
+    "revision": "4287b7b245fb6e7ff043",
+    "url": "/react-plotly/static/js/main.fefae5d9.chunk.js"
+  },
+  {
+    "revision": "68924aa8e5586cbb9818",
+    "url": "/react-plotly/static/js/runtime~main.98e8900d.js"
+  },
+  {
+    "revision": "10c861abf3c0433350ae",
+    "url": "/react-plotly/static/js/2.f6dad15d.chunk.js"
+  },
+  {
+    "revision": "210649580f05d783bc00",
+    "url": "/react-plotly/static/js/3.33f1961e.chunk.js"
+  },
+  {
+    "revision": "26e4cb834757a4cdfc62ca33d54ac9cf",
+    "url": "/react-plotly/index.html"
+  }
+];
