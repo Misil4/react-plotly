5ee64a05a443c752f43479ff65726c725efe04e4
Misil <carlossilvamarques.cs@gmail.com>
2021-11-18 14:57:26 +0100

    (UPDATE) updated routes

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const App = () => {
       <Route path="/react-plotly/Recharts">
       <Recharts />
       </Route>
-      <Route path="/">
+      <Route path="/react-plotly/">
         <Home />
       </Route>
     </Switch>
