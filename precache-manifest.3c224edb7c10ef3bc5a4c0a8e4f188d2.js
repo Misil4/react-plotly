fea81da0810ca5b29cb4c3a6df245770dd41f6f3
Misil <carlossilvamarques.cs@gmail.com>
2021-11-18 15:04:27 +0100

    Updates

---
+++ precache-manifest.3c224edb7c10ef3bc5a4c0a8e4f188d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fa4e0442586cf9db7ef",
+    "url": "/react-plotly/static/css/main.4379803a.chunk.css"
+  },
+  {
+    "revision": "8fa4e0442586cf9db7ef",
+    "url": "/react-plotly/static/js/main.c3aed455.chunk.js"
+  },
+  {
+    "revision": "68924aa8e5586cbb9818",
+    "url": "/react-plotly/static/js/runtime~main.98e8900d.js"
+  },
+  {
+    "revision": "31f7a37e54617692bc34",
+    "url": "/react-plotly/static/js/2.1cb5385e.chunk.js"
+  },
+  {
+    "revision": "210649580f05d783bc00",
+    "url": "/react-plotly/static/js/3.33f1961e.chunk.js"
+  },
+  {
+    "revision": "703acfa05ace5a4f59aed5c8444a6345",
+    "url": "/react-plotly/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-plotly/precache-manifest.7827dfaab066ae9102853275a052ae17.js"
+  "/react-plotly/precache-manifest.3c224edb7c10ef3bc5a4c0a8e4f188d2.js"
 );
 
 workbox.clientsClaim();
