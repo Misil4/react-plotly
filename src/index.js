488a877d14d91ae79bd0f1911fa27396ded0aa34
Misil <carlossilvamarques.cs@gmail.com>
2021-11-18 15:01:55 +0100

    (UPDATE) added Hashrouter

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App.js';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <HashRouter>
     <App />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
